57872b74f7ab07e35fb33effea590e8a41dfbf30
hyungjun-tech01 <whmoon00@naver.com>
2023-01-14 17:11:09 +0900

    typescript inital 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled, {keyframes}  from "styled-components";
+
+const Title = styled.h1`
+  color:${props=>props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  height:100vh;
+  width:100vh;
+  justify-content:center;
+  align-item:center;
+  background-color:${props=>props.theme.backgroundColor};
+  display :flex ;
+  `;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>  
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 
+const darkTheme = {
+  textColor:"whitesmoke",
+  backgroundColor:"#111",
+};
+const lightTheme = {
+  textColor:"#111",
+  backgroundColor:"whitesmoke",
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
