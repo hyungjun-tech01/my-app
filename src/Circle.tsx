d49422f34f8cd1ccfe23996e4c4f90cac5662345
hyungjun-tech01 <whmoon00@naver.com>
2023-01-15 14:10:11 +0900

    typescript optional, default 적용

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
+// css에선 optional 로 만들수가 없다. 
 interface ContainerProps{
     bgColor : string;
+    borderColor : string;
 };
 
 // tag -> styled component 로 한번 더 전달 
@@ -10,17 +12,23 @@ const Container = styled.div<ContainerProps>`
     height : 100px;
     background-color : ${props => props.bgColor };
     border-radius : 50px;
+    border : 2px solid ${props=>props.borderColor};
 `;
 
 // interface : typescript 에서 PropTypes와 비슷한 정의를 할 수 있는 놈/ 
+// ? : optional prop 표시 
 interface CircleProps {
     bgColor : string;
+    borderColor?:string;
+    text?:string;
 };
 
-
-function Circle({bgColor}:CircleProps) {
+// ?? : default 로 주는 값 : 이게 있어야 optional 시 에러가 안남 . 
+// aa = "  " : default 줄 때  
+function Circle({bgColor, borderColor, text="default text"}:CircleProps) {
+    console.log(borderColor);
     return (
-        <Container bgColor={bgColor}/>
+        <Container bgColor={bgColor} borderColor={borderColor?? bgColor}>{text}</Container>
     )
 }
 export default Circle;
