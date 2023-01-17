453b6f8dab3e6083fe42eb8a7f97c2ecaabccfba
hyungjun <hyungjun@sindoh.com>
2023-01-17 17:26:39 +0900

    Coin main 화면 작성중

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,58 @@
 import styled from "styled-components";
+
+const coins = [ 
+    {
+        id: "btc-bitcoin",
+        name: "Bitcoin",
+        symbol: "BTC",
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+        },
+        {
+        id: "eth-ethereum",
+        name: "Ethereum",
+        symbol: "ETH",
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+        },
+        {
+        id: "hex-hex",
+        name: "HEX",
+        symbol: "HEX",
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: "token",
+        },
+    ]
+const Container = styled.div``;
+const Header = styled.header`
+    height : 10vh;
+    display : flex;
+ `;
+const CoinList = styled.ul``;
+const Coin = styled.li``;
 const Title = styled.h1`
 color: ${props=>props.theme.accentColor}
 `;
 
+
 function Coins(){
-    return <Title>Coins</Title>
+    return (
+        <Container>
+            <Header>
+            <Title>Coins</Title>
+            </Header>
+            <CoinList>
+                {coins.map(coin =><Coin key={coin.id}>{coin.name} {coin.symbol}</Coin> )}
+               
+            </CoinList>
+        </Container>
+    );
+ 
 }
 export default Coins;
