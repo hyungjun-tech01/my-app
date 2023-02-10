41b81af45bb3dafd4048ca4db84adb8af5e9ce11
hyungjun <hyungjun@sindoh.com>
2023-02-10 17:26:55 +0900

    react hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
 
-function ToDoList(){
-    // input tag 안에 value 값이 들어오는 것을 감지 하고 써줌 .
-    // event:React.FormEvent<HTMLInputElement> 를 써줌 (이걸 어떻게 알지?? ) 
-    const [value, setValue] = useState("");
-    const changeValue = (event:React.FormEvent<HTMLInputElement>)=>{
-        const { 
-            currentTarget:{value}
-        } = event;
-        //setValue(event.currentTarget.value);
-        setValue(value);
-    }
-    // form을 submit 할 때 동작 value 값을 console에 출력
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();
-        console.log(value);
+// function ToDoList(){
+//     input tag 안에 value 값이 들어오는 것을 감지 하고 써줌 .
+//     event:React.FormEvent<HTMLInputElement> 를 써줌 (이걸 어떻게 알지?? ) 
+//     const [toDo, setToDo] = useState("");
+//     const changeValue = (event:React.FormEvent<HTMLInputElement>)=>{
+//         const { 
+//             currentTarget:{value}
+//         } = event;
+//         setValue(event.currentTarget.value);
+//         setToDo(value);
+//         setToDoError("");
+//     }
+//     toDo Error 처리 
+//     const [toDoError, setToDoError] = useState("");
+//     form을 submit 할 때 동작 value 값을 console에 출력
+//     const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
+//         event.preventDefault();
+//         if(toDo.length < 10){
+//             return setToDoError("toDo should be longer!");
+//         }
+//         console.log(toDo);
+//     }
+//     return (
+//         <div>
+//             <form onSubmit = {onSubmit}>
+//                 <input onChange={changeValue} value = {toDo} placeholder = "write a to do"/>
+//                 <button>Add</button>
+//                 <span>{toDoError === "" ? "" : toDoError}</span>
+//             </form>
+//         </div>
 
-    }
+//     );
+// }
+function ToDoList(){
     return (
         <div>
-            <form onSubmit = {onSubmit}>
-                <input onChange={changeValue} value = {value} placeholder = "write a to do"/>
+            <form>
+                <input  placeholder = "write a to do"/>
                 <button>Add</button>
             </form>
         </div>
-
-    );
+    
+    );    
 }
 export default ToDoList;
