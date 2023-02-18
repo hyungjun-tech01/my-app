cbb24d306a0394585c760ba167aec5e8573e4b77
hyungjun-tech01 <whmoon00@naver.com>
2023-02-18 22:44:22 +0900

    recoil 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import {useForm} from "react-hook-form";
+import {atom, useRecoilState} from "recoil";
 
+const toDoState = atom<IToDo[]>(
+    {
+        key:"toDo",
+        default:[],
+    }
+);
 interface IForm{
     toDo:string,
 }
+interface IToDo{
+    text:string;
+    id:number;
+    category:"TO_DO"|"DOING"|"DONE";
+}
 function ToDoList(){
+    //atom 생성 toDoState 
+    const [toDos, setToDos] = useRecoilState(toDoState);
     //register : onchange value 등을 , onclur, onclick 등을 지원 
     const {register, handleSubmit, formState:{errors}, setValue} = useForm<IForm>();
 
     //onValid엣 사용자 정의 message 추가 
     const onValid = (data:IForm)=>{
-        console.log("data", data);
+        console.log("data", data.toDo);
+        setToDos(oldToDos => [{text:data.toDo, id:Date.now(), category:"TO_DO"}, ...oldToDos]);
         setValue("toDo","");
     }
 
@@ -29,6 +44,9 @@ function ToDoList(){
                 </span>                       
                 <button>Add</button>
             </form>
+            <ul>
+                {toDos.map(toDo=><li key={toDo.id}>{toDo.text}</li> )}
+            </ul>
         </div>
     );    
 }
