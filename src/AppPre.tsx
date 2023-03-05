8f7f163fcfe3d2cdfc7a9640125960a2a0728408
hyungjun-tech01 <whmoon00@naver.com>
2023-03-05 09:02:21 +0900

    framer motion layout id 연결

--- src/AppPre.tsx
+++ src/AppPre.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {motion, AnimatePresence} from "framer-motion";
 import {useState} from "react";
+import { networkInterfaces } from "os";
 
 const Wrapper = styled(motion.div)`
   display : flex;
@@ -11,10 +12,18 @@ const Wrapper = styled(motion.div)`
 `;
 //    justify-content:center;
 //align-items:center;
-
+const Grid = styled.div`
+    display : grid;
+    grid-template-columns:repeat(3,1fr);
+    width:50vw;
+    gap: 10px;
+    div:first-child,
+    div:last-child{
+        grid-column : span 2;
+    }
+`;
 const Box = styled(motion.div)`
-    width : 400px;
-    height : 400px;
+    height : 200px;
     background-color : rgba(255,255,255,1);
     border-radius: 10px;
     display:flex;
@@ -22,26 +31,30 @@ const Box = styled(motion.div)`
     align-items:center;
     box-shadow : 0 20px 3px rgba(0,0,0,0.1), 0 0px 20px 
 `;
-const Circle = styled(motion.div)`
-    width : 100px;
-    height : 100px;
-    border-radius : 50px;    
-    background-color : blue;
-    box-shadow : 0 20px 3px rgba(0,0,0,0.1), 0 0px 20px 
+const Overlay = styled(motion.div)`
+    width:100%;
+    height: 100%;
+    background-color:rgba(0,0,0,0.5);
+    position:absolute;
+    display:flex;
+    justify-content:center;
+    align-items:center;    
 `;
 function AppPre(){
-    const [clicked, setClicked] = useState(false);
-    const toggleClicked = ()=>{
-        setClicked(prev => !prev);
-    };
+    const [lId, setlId] = useState<null|string>(null);
     return(
-        <Wrapper onClick={toggleClicked}>
-            <Box>
-                {clicked ? null:<Circle layoutId="circle" />}
-            </Box>
-            <Box>
-                {clicked ? <Circle layoutId="circle"/>:null} 
-            </Box>                    
+        <Wrapper >
+        <Grid>
+        {["1","2","3","4"].map(n => <Box onClick={()=>setlId(n)} key={n} layoutId={n}></Box>)}
+        </Grid>    
+        <AnimatePresence>{lId? 
+            <Overlay initial={{opacity:0}} 
+                    animate={{opacity:1}} 
+                    exit={{opacity:0}}>
+                    <Box onClick={()=>setlId(null)} layoutId={lId} style={{width:200, height:200 }}/>
+            </Overlay>
+            :null}
+        </AnimatePresence>        
         </Wrapper>            
     );
 }
